9f2983f74651fe9542fad8e935138eff95936ef3
ImanG <igeghellache@gmail.com>
2022-11-27 16:37:44 +0000

    working clock

--- script.js
+++ script.js
@@ -40,9 +40,11 @@ function change() {
       playpause.classList.toggle('playing');
       clearInterval(Interval);
       Interval = setInterval(startTimer,10);
+      startClock();
     } else {
       x.innerHTML = "Play";
       clearInterval(Interval);
+      pauseClock();
     }
   }
 
@@ -115,9 +117,9 @@ function lap()  {
     lapCount ++;
     //anything inside the template literal/placeholder is treated as Javascript
     //I used ternary operators to preserve the format of the time. E.g. 0:0:12:59 was previously shown
-    lapNow = `<div class="lap">Lap ${lapCount}: ${hours <= 9 ? "0" + hours: hours}:${minutes <= 9 ? "0" + minutes: minutes}:${seconds <= 9 ? "0" + seconds: seconds}:${centiseconds <= 9 ? "0" + centiseconds: centiseconds}</div>`;
+    lapNow = `<div class="laptime" >Lap ${lapCount}: ${hours <= 9 ? "0" + hours: hours}:${minutes <= 9 ? "0" + minutes: minutes}:${seconds <= 9 ? "0" + seconds: seconds}:${centiseconds <= 9 ? "0" + centiseconds: centiseconds}</div>`;
     laps.innerHTML += lapNow;
-    console.log(laps);
+    //console.log(laps);
 
     //Using localstorage for laps
     localStorage.setItem('laps', (laps.textContent));
@@ -183,6 +185,78 @@ if (currentTheme) {
 	switchModeBtn.textContent = currentTheme;
 }
 
-//clock
+//-------------------------------------------
 
+// var startTimerButton = document.querySelector('.startTimer');
+// var pauseTimerButton = document.querySelector('.pauseTimer');
+var timerDisplay = document.querySelector('.timer');
+var startTime;
+var updatedTime;
+var difference;
+var tInterval;
+var savedTime;
+var paused = 0;
+var running = 0;
+function startClock(){
+  if(!running){
+    startTime = new Date().getTime();
+    tInterval = setInterval(getShowTime, 1);
+// change 1 to 1000 above to run script every second instead of every millisecond. one other change will be needed in the getShowTime() function below for this to work. see comment there.   
+ 
+    paused = 0;
+    running = 1;
+
+  }
+}
+function pauseClock(){
+  if (!difference){
+    // if timer never started, don't allow pause button to do anything
+  } else if (!paused) {
+    clearInterval(tInterval);
+    savedTime = difference;
+    paused = 1;
+    running = 0;
+  } else {
+// if the timer was already paused, when they click pause again, start the timer again
+startTimer();
+  }
+}
+function resetClock(){
+  clearInterval(tInterval);
+  savedTime = 0;
+  difference = 0;
+  paused = 0;
+  running = 0;
+  //the reset in your function would reset everything
+}
+function getShowTime(){
+  updatedTime = new Date().getTime();
+  if (savedTime){
+    difference = (updatedTime - startTime) + savedTime;
+  } else {
+    difference =  updatedTime - startTime;
+  }
+  // var days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((difference % (1000 * 60)) / 1000);
+  var cseconds = Math.floor((difference % (1000 * 60)) / 10);
+
+	const deg = 6;
+	const hour = document.querySelector(".hour");
+	const min = document.querySelector(".min");
+	const sec = document.querySelector(".sec");
+
+//const setClock = () => {
+	let day = new Date();
+	let hh = hours * 30;
+	let mm = minutes * deg;
+	let ss = seconds * deg;
+
+	hour.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+	min.style.transform = `rotateZ(${mm}deg)`;
+	sec.style.transform = `rotateZ(${ss}deg)`;
+};
+
+//test could be rotate the hour
 
